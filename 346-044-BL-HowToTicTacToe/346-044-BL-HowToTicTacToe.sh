96540c9d6d2897c5add39a588d48a79bd5c1fe94
Fabrice Thut <fabrice.thut@bbbaden.ch>
2025-11-18 12:48:11 +0100

    Replace Node.js Tic Tac Toe app with Docker setup script
    Removed the Node.js-based Tic Tac Toe application, including Dockerfile, server code, frontend, and dependencies. Added a shell script and docker-compose.yml for initializing and deploying a new Docker-based photo album service stack.

---
+++ 346-044-BL-HowToTicTacToe/346-044-BL-HowToTicTacToe.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Initialise the system..."
+
+# --- System aktualisieren ---
+dnf update -y
+
+# --- Docker installieren ---
+dnf install -y git
+
+echo "Clone git repo ..."
+cd /home/ec2-user
+git clone https://github.com/BBBaden/M346.git
+echo "... git repo cloned"
+
+SCRIPTDIR="/home/ec2-user/M346/Common"
+
+# Install Docker
+sh "$SCRIPTDIR/install-docker.sh"
+
+# Create working directory
+WORKDIR="/home/ec2-user/working-directory"
+mkdir -p "$WORKDIR"
+
+# Move files to working-directory
+LADIR="/home/ec2-user/M346/LA_346_0024_GA_UseCaseEC2"
+
+mv "$LADIR/.env.base" "$WORKDIR"
+mv "$LADIR/docker-compose.yml" "$WORKDIR"
+mv "$SCRIPTDIR/m346-docker.service" "$WORKDIR"
+
+#Generate .env
+cd "$WORKDIR"
+sh "$SCRIPTDIR/generate-prod-env.sh"
+
+#Configure Service
+cd "$WORKDIR"
+sh "$SCRIPTDIR/configer-service.sh"
+
+echo "... system initialised"
