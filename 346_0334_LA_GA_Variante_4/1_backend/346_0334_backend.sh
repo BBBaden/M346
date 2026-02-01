73555ae53a1af68df39f67eba96db384b4669961
thutfabrice <126490151+thutfabrice@users.noreply.github.com>
2026-02-01 17:06:53 +0100

    Replace MYSQL_ROOT_PASSWORD with MYSQL_USER_PASSWORD

--- 346_0334_LA_GA_Variante_4/1_backend/346_0334_backend.sh
+++ 346_0334_LA_GA_Variante_4/1_backend/346_0334_backend.sh
@@ -4,8 +4,8 @@
 # --- Variable definieren ---
 # Hier wird die IP gesetzt.
 export MYSQL_DB_ADDRESS="127.0.0.1"
-export MYSQL_ROOT_PASSWORD="rootpass"
 export MYSQL_USER="admin"
+export MYSQL_USER_PASSWORD="rootpass"
 export BUCKET_NAME="m346-variante4-version2"
 
 echo "Initialise the system..."
@@ -39,7 +39,7 @@ mv "$SCRIPTDIR/m346-docker.service" "$WORKDIR"
 # --- Platzhalter ersetzen ---
 # Ersetzt exakt den String ${...} durch den Inhalt der Variable
 sed -i "s/\${MYSQL_DB_ADDRESS}/$MYSQL_DB_ADDRESS/g" "$WORKDIR/.env"
-sed -i "s/\${MYSQL_ROOT_PASSWORD}/$MYSQL_ROOT_PASSWORD/g" "$WORKDIR/.env"
+sed -i "s/\${MYSQL_ROOT_PASSWORD}/$MYSQL_USER_PASSWORD/g" "$WORKDIR/.env"
 sed -i "s/\${MYSQL_USER}/$MYSQL_USER/g" "$WORKDIR/.env"
 sed -i "s/\${BUCKET_NAME}/$BUCKET_NAME/g" "$WORKDIR/.env"
 
